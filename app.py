dc783c7ea17a1f4ede16978932e70ef47a92c8a2
mattycraps <matt.connell02@gmail.com>
2022-01-09 12:48:28 -0500

    finish USD to CAD

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from views import views
 
 app = Flask(__name__)
+#all the routes will be stored in a views.py file by using the following statement
 app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == '__main__':
-    app.run(debug = True, port=4269)  
+    app.run(debug = True, port=5001)  
--- views.py
+++ views.py
@@ -1,31 +1,55 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 
+#this statement allows all routes to be stored in a sepreate python file
 views = Blueprint(__name__, "views")
 
 
+#assigning routes that link to the same home page
 @views.route("/")
+@views.route("home")
+@views.route("/index")
 def home():
     return render_template("index.html")
 
+
+#the route for Cad -> Usd conversion
 @views.route("/CADtoUSD")
 def cad_to_usd():
+    #assign name variable to current conversion and render html file
     return render_template("CADtoUSD.html", name="CAD to USD")
 
+#this route is for when conversion button is clicked in html file
 @views.route("/resultCADtoUSD",methods=['POST', "GET"])
 def resultCADtoUSD():
     output = request.form.to_dict()
+    #typecast output to a float variable
     temp = float(output["value"])
+    #format to 2 decimal places
+    format_currency = "{:.2f}".format(temp)
+    #conversion calculation
     result = 0.79 * temp
-    return render_template("CADtoUSD.html", name=result)
+    #format to 2 decimal places
+    format_float = "{:.2f}".format(result)
+    #assign variables the constant and result of calculation
+    return render_template("CADtoUSD.html", result=format_float, currency=format_currency)
 
+#the route for USD to CAD conversion
+@views.route("/USDtoCAD")
+def usd_to_cad():
+    #assign name variable to current conversion and render html file
+    return render_template("USDtoCAD.html")
 
-#@views.route("/USDtoCAD")
-#def cad_to_usd():
-    #return render_template("USDtoCAD.html")
-
-#@views.route("/resultUSDtoCAD",methods=['POST',"GET"])
-#def resultUSDtoCAD():
-    #output = request.form.to_dict()
-    #temp = float(output["value"])
-    #result = 1.26 * temp
-    #return render_template("USDtoCAD.html", name=result)    
+#this route is for when conversion button is clicked in html file
+@views.route("/resultUSDtoCAD",methods=['POST',"GET"])
+def resultUSDtoCAD():
+    output = request.form.to_dict()
+    #type cast to float variable
+    temp = float(output["value"])
+    #format number to 2 decimal places
+    format_currency = "{:.2f}".format(temp)
+    #conversion calculation
+    result = 1.26 * temp
+    #format number to 2 decimal places
+    format_float = "{:.2f}".format(result)
+    #assign variables and render html file
+    return render_template("USDtoCAD.html", result=format_float, currency=format_currency)    
