d244c34f517e442b5138d7292da83ba1be17c942
mattycraps <matt.connell02@gmail.com>
2022-01-08 20:38:28 -0500

    CAD -> USD works
    usd -> cad not so much sadly

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function submitCADtoUSD(){
+    var number = document.getElementById("CADtoUSD").ariaValueMax;
+    console.log(number);
+    var temp = document.createElement("Input");
+    temp.setAttribute("placeholder", "you're done");
+    document.body.appendChild(temp);
+}
--- views.py
+++ views.py
@@ -5,8 +5,27 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Matthew")
+    return render_template("index.html")
 
 @views.route("/CADtoUSD")
 def cad_to_usd():
     return render_template("CADtoUSD.html", name="CAD to USD")
+
+@views.route("/resultCADtoUSD",methods=['POST', "GET"])
+def resultCADtoUSD():
+    output = request.form.to_dict()
+    temp = float(output["value"])
+    result = 0.79 * temp
+    return render_template("CADtoUSD.html", name=result)
+
+
+#@views.route("/USDtoCAD")
+#def cad_to_usd():
+    #return render_template("USDtoCAD.html")
+
+#@views.route("/resultUSDtoCAD",methods=['POST',"GET"])
+#def resultUSDtoCAD():
+    #output = request.form.to_dict()
+    #temp = float(output["value"])
+    #result = 1.26 * temp
+    #return render_template("USDtoCAD.html", name=result)    
